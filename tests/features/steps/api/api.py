da84b8cbe554357244175099580168df369d009d
Allan da Silva Barroso <50781919+Abarroso2@users.noreply.github.com>
2022-08-11 08:08:42 -0300

    Revert "Add infrastructure for /scheduler API endpoint"

--- tests/features/steps/api/api.py
+++ tests/features/steps/api/api.py
@@ -9,7 +9,6 @@ from api.policies_endpoint import PoliciesEndpoint
 from api.ping_endpoint import PingEndpoint
 from api.cowsay_endpoint import CowsayEndpoint
 from api.openstack_endpoint import OpenstackEndpoint
-from api.scheduler_endpoint import SchedulerEndpoint
 
 
 class API(object):
@@ -32,7 +31,6 @@ class API(object):
         self.ping = PingEndpoint(self.session)
         self.cowsay = CowsayEndpoint(self.session)
         self.me = MeEndpoint(self.session)
-        self.scheduler = SchedulerEndpoint(self.session)
 
     def update_token(self, access_token: str):
         """
